acf8b9522edf6e1632c648783c30a3c69d11aaeb
Lalit <lalitvijay9480@gmail.com>
2021-08-12 16:20:57 +0530

    fix: colours of note tags

--- lib/get-random-color.tsx
+++ lib/get-random-color.tsx
@@ -1,4 +1,4 @@
-const colours = ['red', 'blue', 'green', 'purple', 'gray']
+const colours = ['red', 'blue', 'green', 'purple', 'gray', 'yellow', 'indigo']
 export default function getRandomColor() {
   return colours[Math.floor(Math.random() * colours.length)]
 }
