245adbd1ca1fc53cf59c149b3b842cd7146a4b6f
Lalit <lalitvijay9480@gmail.com>
2021-08-18 20:28:07 +0530

    feat: add public page feature

---
+++ components/note/PublicNote.tsx
@@ -0,0 +1,89 @@
+// load all highlight.js languages
+import lowlight from 'lowlight'
+
+import { useEditor, EditorContent, ReactNodeViewRenderer } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Image from '@tiptap/extension-image'
+import Highlight from '@tiptap/extension-highlight'
+import Link from '@tiptap/extension-link'
+import Placeholder from '@tiptap/extension-placeholder'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import CodeBlockComponent from './CodeBlockComponent'
+import Iframe from './Iframe'
+import Table from '@tiptap/extension-table'
+import TableRow from '@tiptap/extension-table-row'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+
+const Note: React.FC<{
+  noteContent: string
+  noteId: string
+  isModalOpen: boolean
+}> = ({ noteContent }) => {
+  const CustomTableCell = TableCell.extend({
+    addAttributes() {
+      return {
+        // extend the existing attributes …
+        ...this.parent?.(),
+
+        // and add a new one …
+        backgroundColor: {
+          default: null,
+          parseHTML: (element) => {
+            return {
+              backgroundColor: element.getAttribute('data-background-color'),
+            }
+          },
+          renderHTML: (attributes) => {
+            return {
+              'data-background-color': attributes.backgroundColor,
+              style: `background-color: ${attributes.backgroundColor}`,
+            }
+          },
+        },
+      }
+    },
+  })
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Image,
+      Highlight,
+      Link,
+      Iframe,
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      // Default TableCell
+      // TableCell,
+      // Custom TableCell with backgroundColor attribute
+      CustomTableCell,
+      Placeholder.configure({
+        placeholder: 'Write something amazing...',
+      }),
+      CodeBlockLowlight.extend({
+        addNodeView() {
+          return ReactNodeViewRenderer(CodeBlockComponent)
+        },
+      }).configure({ lowlight }),
+    ],
+    autofocus: false,
+    editable: false,
+    content: noteContent,
+  })
+
+  return (
+    <div className='bg-gray-50'>
+      <div
+        id='paperclip-editor'
+        className='px-6 pb-12 font-sans bg-transparent rounded-lg py-7 focus:outline-none active:outline-none'>
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  )
+}
+
+export default Note
---
+++ pages/p/[publicPageId].tsx
@@ -0,0 +1,84 @@
+import prisma from '@/utils/prisma'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Note from '@/components/note/PublicNote'
+import NoteTag from '@/components/note/NoteTag'
+import Head from 'next/head'
+
+const Page = ({
+  content,
+  heading: noteHeading,
+  description: noteDescription,
+  emoji,
+  tags,
+}) => {
+  return (
+    <div className='min-h-screen px-10 bg-gray-50'>
+      <Head>
+        <title>
+          {noteHeading || 'Note'} {'| ' + noteDescription}
+        </title>
+      </Head>
+      <div className='max-w-5xl pt-16 mx-auto px-7'>
+        <div className='items-start justify-start sm:flex group'>
+          <span className='inline-block mr-5 text-4xl font-bold'>{emoji}</span>
+          <div className='inline-block'>
+            <h1 className='text-4xl font-bold'>{noteHeading}</h1>
+            <div className='mt-4 text-lg text-gray-500'>
+              <h2>{noteDescription} </h2>
+            </div>
+            <div className='mt-3 tags'>
+              {tags.map((tag) => (
+                <NoteTag key={tag} tag={tag} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className='mt-12 bg-white note-container'>
+          <Note noteId='hello' isModalOpen={false} noteContent={content} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    const pageId = params.publicPageId
+    const pageData = await prisma.notes.findUnique({
+      where: {
+        publicId: pageId.toString(),
+      },
+    })
+
+    if (pageData.isPublic !== true) {
+      return {
+        notFound: true,
+      }
+    }
+
+    return {
+      props: {
+        content: pageData.note,
+        heading: pageData.noteHeading,
+        description: pageData.noteDescription,
+        emoji: pageData.emoji,
+        tags: pageData.tags,
+      },
+      revalidate: 10 * 60,
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      notFound: true,
+    }
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export default Page
