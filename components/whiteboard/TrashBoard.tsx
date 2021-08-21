00eb351656f93056c52d34aa3977a109c1c73d19
Lalit <lalitvijay9480@gmail.com>
2021-08-21 19:31:58 +0530

    feat: add trash feature for boards

--- components/whiteboard/TrashBoard.tsx
+++ components/whiteboard/TrashBoard.tsx
@@ -1,74 +1,75 @@
-import { Dialog, Transition } from '@headlessui/react'
-import { Fragment, useState } from 'react'
-import { notes } from '@prisma/client'
-import PublicNote from './PublicNote'
-import axios from 'axios'
-import toast from 'react-hot-toast'
-import { useRouter } from 'next/router'
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
+import { whiteboards } from '@prisma/client';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/router';
 
-const TrashNote: React.FC<{ note: notes; mutate: any; allData: notes[] }> = ({
-  note,
-  mutate,
-  allData,
-}) => {
-  let [isOpen, setIsOpen] = useState(false)
-  const router = useRouter()
+const TrashBoard: React.FC<{
+  board: whiteboards;
+  mutate: any;
+  allData: whiteboards[];
+}> = ({ board, mutate, allData }) => {
+  let [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
   function closeModal() {
-    setIsOpen(false)
+    setIsOpen(false);
   }
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(true);
   }
 
   return (
     <div>
       <div
-        key={note?.id}
+        key={board?.id}
         className='max-w-2xl px-5 py-3 mx-auto my-3 mr-4 border border-gray-600 rounded shadow hover:bg-gray-50'>
-        <h3 className='text-xl font-bold'>{note?.noteHeading}</h3>
-        <p className='mb-5 text-base text-gray-700'>{note?.noteDescription}</p>
+        <h3 className='text-xl font-bold'>{board?.boardName}</h3>
+        <p className='mb-5 text-base text-gray-700'>
+          {board?.boardDescription}
+        </p>
         <button
           className='px-3 py-1 mr-2 bg-gray-900 rounded shadow text-gray-50 focus:ring focus:ring-offset-1 focus:ring-gray-700'
           onClick={openModal}>
-          View Note&apos;s content
+          View Whiteboard&apos;s content
         </button>
         <button
           className='px-3 py-1 mr-2 text-blue-700 bg-blue-200 border border-blue-500 rounded shadow focus:ring focus:ring-offset-1 focus:ring-blue-700'
           onClick={() => {
-            let newData = allData.filter((item) => item.id !== note?.id)
-            console.log(JSON.stringify(newData, null, 2))
-            const removeFromTrash = axios.post('/api/notes/trash', {
-              id: note?.id,
+            let newData = allData.filter((item) => item.id !== board?.id);
+            console.log(JSON.stringify(newData, null, 2));
+            const removeFromTrash = axios.post('/api/whiteboard/trash', {
+              id: board?.id,
               inTrash: false,
-            })
+            });
             // .then(() => {
             //   router.reload()
             // })
             toast.promise(removeFromTrash, {
-              error: `Could not restore ${note?.noteHeading}`,
-              success: 'Note restored from trash successfully.',
-              loading: `Restoring ${note?.noteHeading}...`,
-            })
+              error: `Could not restore ${board?.boardName}`,
+              success: 'Whiteboard restored from trash successfully.',
+              loading: `Restoring ${board?.boardName}...`,
+            });
           }}>
-          Restore Note
+          Restore Whiteboard
         </button>
         <button
           className='px-3 py-1 text-red-700 bg-red-200 border border-red-500 rounded shadow focus:ring focus:ring-offset-1 focus:ring-red-700'
           onClick={() => {
-            const deleteNote = axios.post('/api/notes/delete-note', {
-              id: note?.id,
-            })
+            const deleteBoard = axios.post('/api/whiteboard/delete-board', {
+              id: board?.id,
+            });
             // .then(() => {
             //   router.reload()
             // })
-            toast.promise(deleteNote, {
-              error: 'Error deleting note',
-              success: 'Note deleted successfully',
-              loading: 'Deleting note...',
-            })
+            toast.promise(deleteBoard, {
+              error: 'Error deleting whiteboard',
+              success: 'Whiteboard deleted successfully',
+              loading: 'Deleting whiteboard...',
+            });
           }}>
-          Delete Note
+          Delete Whiteboard
         </button>
       </div>
       <Transition appear show={isOpen} as={Fragment}>
@@ -106,15 +107,22 @@ const TrashNote: React.FC<{ note: notes; mutate: any; allData: notes[] }> = ({
                 <Dialog.Title
                   as='h3'
                   className='text-lg font-medium leading-6 text-gray-900'>
-                  {note?.noteHeading}
+                  {board?.boardName}
                 </Dialog.Title>
                 <div className='mt-2'>
                   <p className='text-sm text-gray-500'>
-                    {note?.noteDescription}
+                    {board?.boardDescription}
                   </p>
                 </div>
                 <div className='my-5'>
-                  <PublicNote noteContent={note?.note} />
+                  <iframe
+                    src={board?.boardUrl}
+                    id='excalidraw-iframe'
+                    frameBorder='0'
+                    allowFullScreen
+                    allowTransparency
+                    className='w-full min-h-[450px] h-full pointer-events-none select-none'
+                  />
                 </div>
                 <div className='mt-4'>
                   <button
@@ -130,7 +138,7 @@ const TrashNote: React.FC<{ note: notes; mutate: any; allData: notes[] }> = ({
         </Dialog>
       </Transition>
     </div>
-  )
-}
+  );
+};
 
-export default TrashNote
+export default TrashBoard;
--- pages/api/trash.ts
+++ pages/api/trash.ts
@@ -1,35 +1,35 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '@/utils/prisma'
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/utils/prisma';
 import {
   withApiAuthRequired,
   UserProfile,
   getSession,
-} from '@auth0/nextjs-auth0'
+} from '@auth0/nextjs-auth0';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { body } = req
   const {
     user: { sub },
-  }: { user: UserProfile } = getSession(req, res)
+  }: { user: UserProfile } = getSession(req, res);
   const trashNotesRequest = prisma.notes.findMany({
     where: {
       createdBy: sub,
       inTrash: true,
     },
-  })
+  });
   const trashBoardsRequest = prisma.whiteboards.findMany({
     where: {
       createdBy: sub,
+      inTrash: true,
     },
-  })
+  });
   const [trashNotes, trashBoards] = await prisma.$transaction([
     trashNotesRequest,
     trashBoardsRequest,
-  ])
+  ]);
   res.json({
     notes: trashNotes,
     boards: trashBoards,
-  })
-}
+  });
+};
 
-export default withApiAuthRequired(handler)
+export default withApiAuthRequired(handler);
