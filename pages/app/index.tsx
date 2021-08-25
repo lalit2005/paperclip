9926951598be4cd67fe743fc7ff238a19f103a0f
Lalit <lalitvijay9480@gmail.com>
2021-08-25 11:42:23 +0530

    feat: add "copy sticky note" feature in dashboard

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -9,6 +9,8 @@ import fetcher from '@/lib/fetcher';
 import Head from 'next/head';
 import { DashboardData } from 'types/types';
 import truncate from 'lodash.truncate';
+import { HiOutlineClipboard } from 'react-icons/hi';
+import toast from 'react-hot-toast';
 
 const indexPage: React.FC<{ user: UserProfile }> = ({ user }) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -66,16 +68,25 @@ const indexPage: React.FC<{ user: UserProfile }> = ({ user }) => {
             <div className='w-full max-w-sm px-4 py-2 mx-auto'>
               <h3 className='my-3 text-lg font-medium'>Your Sticky Notes</h3>
               {data?.stickyNotes?.map((stickyNote) => (
-                <Link key={stickyNote?.id} href={'/app/sticky-notes'}>
-                  <a className='block'>
-                    <div>
+                <div key={stickyNote?.id} className='relative group'>
+                  <Link href={'/app/sticky-notes'}>
+                    <a className='block '>
                       <p className='my-3' style={{ color: stickyNote?.color }}>
-                        {truncate(stickyNote?.stickyNote, { length: 100 })}
+                        {truncate(stickyNote?.stickyNote, { length: 70 })}
                       </p>
-                    </div>
-                  </a>
-                </Link>
-              )) || <Skeleton count={4} className='w-full px-2 py-1 my-1' />}
+                    </a>
+                  </Link>
+                  <div
+                    className='absolute top-0 z-10 hidden px-1 py-px border rounded cursor-pointer group-hover:inline-block group-focus:inline-block right-2 backdrop-filter backdrop-blur-sm bg-white/50'
+                    onClick={() => {
+                      window.navigator.clipboard
+                        .writeText(stickyNote?.stickyNote)
+                        .then(() => toast.success('Copied to clipboard!!'));
+                    }}>
+                    <HiOutlineClipboard className='relative inline-block w-4 h-4 text-gray-600 bottom-[2px]' />
+                  </div>
+                </div>
+              )) || <Skeleton count={4} className='w-full px-2 py-1 my-2' />}
               {data?.stickyNotes?.length == 0 && (
                 <div className='w-full px-2 py-1 my-3'>
                   <p>
@@ -160,5 +171,9 @@ const links = [
   {
     path: '/app/todo',
     title: 'Todolists',
+  },
+  {
+    path: '/app/sticky-notes',
+    title: 'Sticky notes',
   },
 ];
