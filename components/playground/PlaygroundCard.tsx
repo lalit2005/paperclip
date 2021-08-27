4e093081403b919d5906b71d45a63a774bf65207
Lalit <lalitvijay9480@gmail.com>
2021-08-27 19:15:21 +0530

    style: fix playground card's width

--- components/playground/PlaygroundCard.tsx
+++ components/playground/PlaygroundCard.tsx
@@ -9,25 +9,21 @@ const PlaygroundCard: React.FC<{
   playground: CodePlayground | Fuse.FuseResult<CodePlayground>;
 }> = ({ playground }) => {
   return (
-    <div>
-      <Link href={`/app/playgrounds/${playground.id || playground.item.id}`}>
-        <a className='inline-block w-full max-w-xl'>
-          <div className='p-5 my-4 border border-gray-500 rounded shadow-md hover:bg-gray-50'>
-            <h3 className='text-lg text-gray-900'>
-              {playground.playgroundName || playground.item.playgroundName}
-            </h3>
-            <div className='my-5'>
-              {playground?.tags?.map((tag) => (
+    <Link href={`/app/playgrounds/${playground.id || playground.item.id}`}>
+      <a className='inline-block w-full max-w-xl'>
+        <div className='p-5 my-4 border border-gray-500 rounded shadow-md hover:bg-gray-50'>
+          <h3 className='text-lg text-gray-900'>
+            {playground.playgroundName || playground.item.playgroundName}
+          </h3>
+          <div className='my-5'>
+            {playground?.tags?.map((tag) => <NoteTag key={tag} tag={tag} />) ||
+              playground?.item?.tags?.map((tag) => (
                 <NoteTag key={tag} tag={tag} />
-              )) ||
-                playground?.item?.tags?.map((tag) => (
-                  <NoteTag key={tag} tag={tag} />
-                ))}
-            </div>
+              ))}
           </div>
-        </a>
-      </Link>
-    </div>
+        </div>
+      </a>
+    </Link>
   );
 };
 
