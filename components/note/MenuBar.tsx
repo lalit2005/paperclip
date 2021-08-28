09875535fcfb73c76687bb3e29f309898f0899ee
Lalit <lalitvijay9480@gmail.com>
2021-08-28 09:09:59 +0530

    feat: make youtube video draggable

--- components/note/MenuBar.tsx
+++ components/note/MenuBar.tsx
@@ -16,6 +16,7 @@ import {
   HiOutlineExternalLink,
   HiOutlineInformationCircle,
 } from 'react-icons/hi';
+import Draggable from 'react-draggable';
 import * as Portal from '@radix-ui/react-portal';
 
 // load all highlight.js languages
@@ -382,96 +383,85 @@ const MenuBar: React.FC<{ editor: Editor; noteId: string }> = ({
       </div>
       {ytVideo.displayVideo && (
         <Portal.Root>
-          <div
-            className={clsx(
-              'fixed w-[672px] transition-all duration-500 shadow-2xl rounded-br-md',
-              isVideMinimized ? 'h-0' : 'h-[378px]'
-            )}>
-            <div className='flex items-center justify-between w-full px-2 py-1 text-sm text-gray-500 bg-white'>
-              <p className='mr-4'>
-                https://youtube.com/watch?v={ytVideo.url.split('embed/')[1]}
-              </p>
-              <div>
-                <HiOutlineXCircle
-                  onClick={() => {
-                    setYtVideo({
-                      ...ytVideo,
-                      displayVideo: false,
-                    });
-                  }}
-                  title='Close the video'
-                  style={{ zoom: '1.3' }}
-                  className='inline-block w-4 h-4 p-px mx-1 text-red-600 rounded-full cursor-pointer hover:bg-red-100'
-                />
-                {isVideMinimized ? (
-                  <button
-                    className='inline-block'
-                    onClick={() => {
-                      setIsVideMinimized(false);
-                    }}
-                    id='maximize-icon'>
-                    <HiOutlinePlusCircle
-                      title='Maximize the video'
-                      style={{ zoom: '1.3' }}
-                      className='inline-block w-4 h-4 p-px mx-1 text-green-600 rounded-full cursor-pointer hover:bg-green-100'
-                    />
-                  </button>
-                ) : (
-                  <button
-                    className='inline-block'
-                    onClick={() => {
-                      setIsVideMinimized(true);
-                    }}
-                    id='minimize-icon'>
-                    <HiOutlineMinusCircle
-                      title='Minimize the video'
-                      style={{ zoom: '1.3' }}
-                      className='inline-block w-4 h-4 p-px mx-1 text-green-600 transition-all duration-500 rounded-full cursor-pointer hover:bg-green-100'
-                    />
-                  </button>
-                )}
-                {/* <a
-                  href={`https://youtube.com/watch?v=${
-                    ytVideo.url.split('embed/')[1]
-                  }`}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='inline-block'>
-                  <HiOutlineExternalLink
-                    title='Open video in new tab'
-                    style={{ zoom: '1.3' }}
-                    className='inline-block w-4 h-4 p-px mx-1 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100'
-                  />
-                </a> */}
-                <span className='inline-block'>
-                  <CustomTooltip
-                    text='Some shortcuts to boost your productivity\n1. Cmd/Ctrl + M to minimize and maximize the video'
-                    showArrow>
-                    <HiOutlineInformationCircle
-                      title='Some shortcuts to boost your productivity'
-                      style={{ zoom: '1.3' }}
-                      className='inline-block w-4 h-4 p-px mx-1 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100'
-                    />
-                  </CustomTooltip>
-                </span>
-              </div>
-            </div>
-
-            <iframe
-              src={ytVideo.url}
-              frameBorder='0'
-              draggable
-              title='YouTube video player'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-              id='yt-video-iframe'
-              allowFullScreen
+          <Draggable>
+            <div
               className={clsx(
-                'rounded-br-md w-full h-full',
-                isVideMinimized && 'opacity-0'
+                'fixed w-[672px] transition-all duration-500 shadow-2xl rounded-br-md cursor-move',
+                isVideMinimized ? 'h-0' : 'h-[378px]'
               )}>
-              Loading...
-            </iframe>
-          </div>
+              <div className='flex items-center justify-between w-full px-2 py-1 text-sm text-gray-500 bg-white'>
+                <p className='mr-4'>
+                  https://youtube.com/watch?v={ytVideo.url.split('embed/')[1]}
+                </p>
+                <div>
+                  <HiOutlineXCircle
+                    onClick={() => {
+                      setYtVideo({
+                        ...ytVideo,
+                        displayVideo: false,
+                      });
+                    }}
+                    title='Close the video'
+                    style={{ zoom: '1.3' }}
+                    className='inline-block w-4 h-4 p-px mx-1 text-red-600 rounded-full cursor-pointer hover:bg-red-100'
+                  />
+                  {isVideMinimized ? (
+                    <button
+                      className='inline-block'
+                      onClick={() => {
+                        setIsVideMinimized(false);
+                      }}
+                      id='maximize-icon'>
+                      <HiOutlinePlusCircle
+                        title='Maximize the video'
+                        style={{ zoom: '1.3' }}
+                        className='inline-block w-4 h-4 p-px mx-1 text-green-600 rounded-full cursor-pointer hover:bg-green-100'
+                      />
+                    </button>
+                  ) : (
+                    <button
+                      className='inline-block'
+                      onClick={() => {
+                        setIsVideMinimized(true);
+                      }}
+                      id='minimize-icon'>
+                      <HiOutlineMinusCircle
+                        title='Minimize the video'
+                        style={{ zoom: '1.3' }}
+                        className='inline-block w-4 h-4 p-px mx-1 text-green-600 transition-all duration-500 rounded-full cursor-pointer hover:bg-green-100'
+                      />
+                    </button>
+                  )}
+                  <span className='inline-block'>
+                    <CustomTooltip
+                      text='Press Cmd/Ctrl + M to minimize and maximize the video'
+                      showArrow>
+                      <HiOutlineInformationCircle
+                        title='Some shortcuts to boost your productivity'
+                        style={{ zoom: '1.3' }}
+                        className='inline-block w-4 h-4 p-px mx-1 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100'
+                      />
+                    </CustomTooltip>
+                  </span>
+                </div>
+              </div>
+
+              <iframe
+                src={ytVideo.url}
+                frameBorder='0'
+                draggable
+                title='YouTube video player'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                id='yt-video-iframe'
+                allowFullScreen
+                className={clsx(
+                  'rounded-br-md w-full h-full',
+                  isVideMinimized && 'opacity-0'
+                )}>
+                Loading...
+              </iframe>
+            </div>
+          </Draggable>
         </Portal.Root>
       )}
     </div>
