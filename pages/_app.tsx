a010a3b422ec4adccd7cce77b93e60cd681f9569
Lalit <lalitvijay9480@gmail.com>
2021-08-31 23:36:10 +0530

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,17 @@ import '@/styles/globals.css';
 import '@/styles/notes.css';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { NextSeo } from 'next-seo';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <Script
+        strategy='afterInteractive'
+        async
+        defer
+        data-website-id={process.env.NEXT_PUBLIC_ANALYTICS_ID}
+        src={process.env.NEXT_PUBLIC_ANALYTICS_URL}></Script>
       <NextSeo
         title='Paperclip'
         description='The ultimate productivity tool you will ever need. From notes, whiteboards to code playgrounds, Paperclip will be your ultimate workstation'
